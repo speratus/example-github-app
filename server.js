5cca61c0c28bf1638fb1dabe6927fb0c2e4b797a
Glitch (hello-sqlite) <none>
2020-04-28 14:58:28 +0000

    🎻🕹 Checkpoint ./server.js:6482495/271

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
 
 app.post('/webhooks', (request, response) => {
+  console.log(`received webhook with id: ${request.get("X-GitHub-Delivery")} and hash: ${request.get("X-Hub-Signature")}`)
+  console.log(`It's type is: ${request.get('X-GitHub-Event')}`)
   console.log(request.body)
   response.send('OK')
 })
