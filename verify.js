dbd8a45e3ce18c8c600f6044023c5f76d1740272
Glitch (hello-sqlite) <none>
2020-04-28 01:54:02 +0000

    🎉🍳 Checkpoint ./verify.js:6482495/12

--- verify.js
+++ verify.js
@@ -4,6 +4,6 @@ const crypto = require('crypto')
 export function verify_message(message, hash) {
   const hmac = crypto.createHmac('sha1', process.env.WEBHOOK_SECRET)
   hmac.update(message)
-  const digest = hmac.digest('hex')
+  const digest = 'sha1=' + hmac.digest('hex')
   return crypto.timingSafeEqual(hash, digest)
 }
