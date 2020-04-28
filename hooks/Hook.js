8719f883677ac475b245a6f03e85c19b3cccf522
Glitch (hello-sqlite) <none>
2020-04-28 15:21:40 +0000

    🏨🌮 Checkpoint ./hooks/Hook.js:6482495/217

---
+++ hooks/Hook.js
@@ -0,0 +1,12 @@
+
+
+export class Hook {
+  constructor(payload, signature, type, delivery) {
+    this.payload = payload
+    this.signature = signature
+    this.type = type
+    this.delivery = delivery
+  }
+  
+  
+}
