0aa10429e64e4735a57ac384598e2a6d9822ef4b
Glitch (hello-sqlite) <none>
2020-04-29 12:55:22 +0000

    🌼👜 Checkpoint ./server.js:6482495/162 ./views/callback.html:6482495/604 ./package.json:6482495/53

--- server.js
+++ server.js
@@ -17,16 +17,19 @@ app.use(express.static("public"));
 
 // init sqlite db
 const dbFile = "./.data/sqlite.db";
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(dbFile);
-const createHook = require('./hooks/factory')
+// const exists = fs.existsSync(dbFile);
+// const sqlite3 = require("sqlite3").verbose();
+// const db = new sqlite3.Database(dbFile);
 
 app.post('/webhooks', (request, response) => {
   console.log(`received webhook with id: ${request.get("X-GitHub-Delivery")} and hash: ${request.get("X-Hub-Signature")}`)
   console.log(`It's type is: ${request.get('X-GitHub-Event')}`)
   console.log(request.body)
   response.send('OK')
+})
+
+app.get("/callback", (req, res) => {
+  res.sendFile(`${__dirname}/view/callback.html`)
 })
 
 // http://expressjs.com/en/starter/basic-routing.html
