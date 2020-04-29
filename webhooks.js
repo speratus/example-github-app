2faa223b24b184107844d4643b42da63d4424961
Glitch (hello-sqlite) <none>
2020-04-29 14:18:23 +0000

    🚇🍕 Checkpoint ./webhooks.js:6482495/306

--- webhooks.js
+++ webhooks.js
@@ -41,11 +41,20 @@ exports.parseWebhook = (payload, type, signature, delivery) => {
 }
 
 function convertDay(date) {
-  
+  const days = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+  }
+  return days[(new Date(date)).getDay()]
 }
 
 function processPush(data, commitCountData) {
-  
+  let time = Date.now()
 }
 
 exports.processData = (data, context) => {
