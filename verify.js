91105a87a37b2da55912bdac5378180b1e2ab63c
Glitch (hello-sqlite) <none>
2020-04-29 23:44:22 +0000

    🌓👼 Checkpoint ./package.json:6482495/822 ./public/style.css:6482495/910 ./verify.js:6482495/306

--- verify.js
+++ verify.js
@@ -1,9 +1,10 @@
+//Required for safely verifying the integrity of the webhooks
 const crypto = require('crypto')
 
 
 module.exports = function verifyMessage(message, hash) {
-  console.log(`message is: ${typeof message}`)
-  console.log(message)
+  //See the GitHub guide for securing webhooks to understand this function
+  // https://developer.github.com/webhooks/securing/
   const hmac = crypto.createHmac('sha1', process.env.WEBHOOK_SECRET)
   hmac.update(JSON.stringify(message))
   const digest = 'sha1=' + hmac.digest('hex')
