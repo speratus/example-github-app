86161c1d8926aafdc21c2190ca588a054beb95ea
Glitch (hello-sqlite) <none>
2020-04-29 14:42:21 +0000

    🚇💶 Checkpoint ./datemath.js:6482495/386

--- datemath.js
+++ datemath.js
@@ -19,6 +19,19 @@ function daysToTime(days) {
   return days * MILLISECONDS_PER_DAY
 }
 
+function daysUntilNextSunday(date) {
+  return 7 - (new Date(date).getDay())
+}
+
+function nextSunday(date) {
+  let days = daysUntilNextSunday(date)
+  let daysInTime = daysToTime(days)
+  return date + daysInTime
+}
+
+function is
+
+
 exports.milliPerDay = MILLISECONDS_PER_DAY
 exports.convertDay = convertDay
 exports.daysToTime = daysToTime
